f03dd80cba2cc13c510da937746dee94797615a4
Himanshu Arora <himanshuarora9419@gmail.com>
2017-09-21 20:54:44 +0530

    todo api

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+var mongoose=require('mongoose');
+var User =mongoose.model('users',{
+    name:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    }
+});
+module.exports={User};
---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+var {mongoose}=require('./db/mongoose.js');
+var {Todo}=require('./models/todo.js');
+var {User}=require('./models/user.js');
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    console.log(req.body);
+    var todo=new Todo({
+        text : req.body.text
+    });
+    todo.save().then((doc)=>{
+    res.send(doc);
+    },(e)=>{
+    res.status(400).send(e);
+})
+
+})
+
+app.listen(3000,()=>{
+    console.log("at port");
+});
+
+// var newTodo= new Todo({
+//     text:'cooked food', completed:true,completedAt:1000
+// })
+
+// newTodo.save().then((doc)=>{
+//     console.log('saved to do',doc)
+// },(e)=>{
+//     console.log("error")
+// })
+
+
+// var newUser=new User({
+//     name:"himanshu",
+//     email:"himanshu@gmail.com"
+// })
+// newUser.save().then(
+//     (doc)=>{
+//         console.log(doc)
+//     },(error)=>{
+//         console.log("some error",error)
+//     })
