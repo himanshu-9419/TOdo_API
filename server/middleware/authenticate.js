ecff39a5d0941b4b7d73f87c79b1f83f67f07208
Himanshu Arora <himanshuarora9419@gmail.com>
2017-09-29 10:00:15 +0530

    add authenticate middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,15 @@
+var {User}=require('./../models/user.js');
+var authenticate=function(req,res,next){
+    var token=req.header('x-auth');
+    User.findByToken(token).then((user)=>{
+        if(!user){ return Promise.reject();}
+        //res.send(user);
+        req.user=user;
+        req.token=token;
+        next();
+    }).catch((e)=>{
+        res.status(401).send();
+    });
+}
+
+module.exports = {authenticate};
